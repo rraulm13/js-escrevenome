8ff7f87ac8a35e79ec8030e2359dcecace719df3
rraulm13 <140711948+rraulm13@users.noreply.github.com>
2023-08-03 16:09:45 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+}
+
+function draw() {
+    stroke("blue");
+    fill("red");
+
+    1f(mouseIsPressed){
+        Reflect(mouseX, mouseY, 20, 35);
+    }
+
+}
+
